71e062816d5d81c9410becb9825bc91c7b77de74
Carlos Faustino <chfcchfc96@gmail.com>
2021-06-22 09:46:19 -0300

    .

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -18,5 +18,7 @@ const firebaseConfig = {
 // INICIALIZAR O APP COM AS CONFIGURAÇÕES DO FIREBASE
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const database = firebase.database();
+const auth = firebase.auth();
+const database = firebase.database();
+
+export { firebase, auth, database };
