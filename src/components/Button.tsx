c899fc7f3cd2bb8364cfd126bcffe112c708e0ac
Carlos Faustino <chfcchfc96@gmail.com>
2021-06-24 09:44:20 -0300

    Botão recebe uma prop para alterar estilização

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,8 +2,12 @@ import { ButtonHTMLAttributes } from 'react';
 
 import '../styles/button.scss';
 
-export const Button = (props: ButtonHTMLAttributes<HTMLButtonElement>) => {
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean;
+};
+
+export const Button = ({ isOutlined = false, ...props }: ButtonProps) => {
   return (
-    <button className="button" {...props} />
+    <button className={`button ${isOutlined ? 'outlined' : ''}`} {...props} />
   );
 };
